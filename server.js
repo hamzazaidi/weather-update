a8a6f42b63050583e425d1af4185ea8d3803811f
Hamza Zaidi <humza.zaidi@gmail.com>
2021-06-27 16:32:57 -0400

    add build scripts

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-//const cityList = require('./src/city.list.json')
+const cityList = require('./src/city.list.json')
 
 app.use(cors());
 
@@ -21,12 +21,11 @@ app.get('/details', async (req, res) => {
 })
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function (req, res) {
-
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
