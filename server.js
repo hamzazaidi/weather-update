df05bb452f25d04425f560e3bb360895f43e3f81
Hamza Zaidi <humza.zaidi@gmail.com>
2021-06-27 20:38:20 -0400

    add build scripts

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.get('/details', async (req, res) => {
 })
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join('/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
